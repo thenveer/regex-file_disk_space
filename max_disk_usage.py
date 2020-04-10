b66db6b6ca7e610663d26ecd66ce2597d6000bea
muhamed <thenveer81@gmail.com>
2020-04-10 23:14:15 +0530

    first commit

---
+++ max_disk_usage.py
@@ -0,0 +1,27 @@
+import subprocess
+import re
+import sys
+
+def get_disk_space(filename): 
+  with open(filename, 'w+') as f:
+    df = subprocess.run('df', stdout=f, stderr=subprocess.DEVNULL)
+def get_file_max_disk_usage(fname):
+    get_disk_space(fname)
+    filesystem=[]
+    with open(fname) as f:
+        for i in f:
+            #print(i)
+            p1= re.compile('^.*100%.*$')
+            if p1.match(i):
+                #print(i)
+                Filesystem, Size, Used, Avail, Use, Mountedno =i.split()
+                #print(Filesystem)
+                filesystem.append(Filesystem)
+        return filesystem
+
+
+def main():
+    a=get_file_max_disk_usage(sys.argv[1])
+    print(a)
+if __name__ == "__main__":
+    main()
